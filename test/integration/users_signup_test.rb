fae552fca34fdeaf27639ea30b320287b8e43eb3
yongwoon <yonyon.japan@gmail.com>
2017-12-20 11:13:35 +0900

    11.2.4 Updating the Users create action

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -11,8 +10,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    # assert_select 'div#<CSS id for error explanation>'
-    # assert_select 'div.<CSS class for field with error>'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
 
   test "valid signup information" do
@@ -24,8 +23,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "password" } }
     end
     follow_redirect!
-    assert_template 'users/show'
-    # assert_not flash.FILL_IN
-    assert is_logged_in?
+    # assert_template 'users/show'
+    # assert is_logged_in?
   end
 end
