f0aa632525d4df510e1cc3fdb0bc3cc19dc316a4
yongwoon <yonyon.japan@gmail.com>
2017-12-20 11:00:49 +0900

    11.1.2 Account activation data model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
 
   before_save { email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 }
@@ -39,5 +41,16 @@ class User < ApplicationRecord
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  # Converts email to all lower-case.
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # Creates and assigns the activation token and digest.
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
